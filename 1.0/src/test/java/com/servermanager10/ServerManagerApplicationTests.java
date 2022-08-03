dd3540519808ea56a6e0204c7d52438a91462946
justin.jarrard75@gmail.com <justin.jarrard75@gmail.com>
2022-08-02 20:45:07 -0500

    Initialized project via spring initializr

---
+++ 1.0/src/test/java/com/servermanager10/ServerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servermanager10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
